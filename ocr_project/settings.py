7d45277ef4bcce7b51b285a16d6e1ac5777b6c5a
shahid <shahid.ice.nstu@gmail.com>
2022-05-23 15:33:39 +0600

    install django extensions

--- ocr_project/settings.py
+++ ocr_project/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     # third party apps
     'crispy_forms',
     'rest_framework',
+    'django_extensions',
     # local apps
     'reading_app',
     'digit_recognizer.apps.DigitRecognizerConfig',
@@ -86,20 +87,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
