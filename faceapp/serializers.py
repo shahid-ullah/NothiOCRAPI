0a6d1b20a47ed8928589deb2ebf6b9fbd1bd5ebf
shahid <shahid.ice.nstu@gmail.com>
2022-06-05 09:16:24 +0600

    handle base64 image face compare api

--- faceapp/serializers.py
+++ faceapp/serializers.py
@@ -1,7 +1,8 @@
 # faceapp/serializers.py
+from drf_base64.fields import Base64ImageField
 from rest_framework import serializers
 
 
 class FaceCompareSerializer(serializers.Serializer):
-    nidcard_image = serializers.ImageField()
-    webcam_image = serializers.ImageField()
+    nidcard_image = Base64ImageField()
+    webcam_image = Base64ImageField()
